f7c5213b89a80638e61841a7d79f258b11c64c6e
angelchiav <achiavicuna@gmail.com>
2025-11-04 00:13:48 -0300

    new type files and new methods

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"regexp"
+	"strings"
 )
 
 func cleanInput(text string) []string {
@@ -10,6 +13,29 @@ func cleanInput(text string) []string {
 	return re.FindAllString(text, -1)
 }
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
 func main() {
-	fmt.Println(cleanInput("La mamá de la mamá"))
+	reader := os.Stdin
+	scanner := bufio.NewScanner(reader)
+	for {
+
+		fmt.Printf("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		text := cleanInput(scanner.Text())
+
+		if len(text) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %v\n", strings.ToLower(text[0]))
+	}
 }
--- repl_test.go
+++ repl_test.go
@@ -15,8 +15,8 @@ func TestCleanInput(t *testing.T) {
 		},
 
 		{
-			input:    "Rihanna, banana,    manzana",
-			expected: []string{"Rihanna", "banana", "manzana"},
+			input:    "Rihanna, banana,    katana ain't no mountain high enough",
+			expected: []string{"Rihanna", "banana", "katana", "ain't", "no", "mountain", "high", "enough"},
 		},
 	}
 
